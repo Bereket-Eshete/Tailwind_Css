220fa91340c184b46440687b7978c7a60fd28eaa
Bereket-Eshete <bereketeshete63@gmail.com>
2025-06-21 15:04:13 +0300

    Chapter 3: Project Pseudo-Classes, Forms & Footer

--- Project/tailwind.config.js
+++ Project/tailwind.config.js
@@ -2,7 +2,12 @@
 export default {
   content: ["./build/*.html"],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        widescreen: { raw: "(min-aspect-ratio: 3/2)" },
+        tallscreen: { raw: "(min-aspect-ratio: 1/2)" },
+      },
+    },
   },
   plugins: [],
 };
